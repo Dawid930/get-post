a593e7e2d6cf1c1db01c9b8866432a764246d5ca
Dawid930 <davidnyikes@gmail.com>
2022-03-26 16:45:10 +0100

    script updated

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,3 +1,28 @@
+                                        // map- async-await-el
+ 
+const loadEvent = async () =>{
+    const rootElement = document.getElementById("root");
+    const beersObjData = await fetch("public/data.json");
+    const beersObj = await beersObjData.json();
+
+    const beerHTML = beersObj.cards.map(x => `
+    <div class="beer">
+        <h2>${x.title}</h2>
+        <h4>${x.sub}</h4>
+        <p>${x.text}</p>
+    </div>
+    `);
+
+    console.log(beerHTML);
+
+    rootElement.insertAdjacentHTML('beforeend', beerHTML.join(''))  // join nelkul a .map arrayt dob vissza, amit nem jol fog megjeleniteni a bongeszo, mert string kell neki. A .join stringet ad vissza!!
+
+}
+
+window.addEventListener("load", loadEvent)
+
+/* 
+                                        //for ciklussal
 fetch("./public/data.json")
     .then(response => response.json())
     .then(data =>{
@@ -12,3 +37,4 @@ fetch("./public/data.json")
         }
     })
   
+ */
